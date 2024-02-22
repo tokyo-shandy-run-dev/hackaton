a19bfc7757ebc5d2ff5314cf9a21f983e8bf3c67
cou <courange.c@gmail.com>
2024-02-22 15:59:56 +0900

    add schema

--- src/lib/hello.ts
+++ src/lib/hello.ts
@@ -1,3 +1,11 @@
-export function hello() {
+import { Prisma, PrismaClient, Project, ProjectOnUser, TimeState, User } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function hello() {
+  const res = await fetch("api/project", { method: "GET" });
+  if (!res.ok) throw new Error();
+  const body = await res.json();
+  body;
   return "hello";
 }
